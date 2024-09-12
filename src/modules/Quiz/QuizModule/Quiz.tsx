46cf52ee95f5f62d4c992d68d3488d1ce27b7ba6
Simon Jones <simon@thedelta.io>
2024-09-12 19:31:14 +0100

    Content dynamic based on URL queryParam

--- src/modules/Quiz/QuizModule/Quiz.tsx
+++ src/modules/Quiz/QuizModule/Quiz.tsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
 import QuestionLayout from '../../../components/QuestionLayout';
 import { quizData } from '../../../mockData/madrssData';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const Quiz = () => {
-  return <QuestionLayout itemInfo={quizData[0]} questionNumber={1} />;
+  const { questionNumber } = useParams();
+  const questionNumberInt = parseInt(questionNumber ?? '1', 10);
+
+  return (
+    <>
+      <QuestionLayout
+        itemInfo={quizData[questionNumberInt - 1]}
+        questionNumber={questionNumberInt}
+      />
+      <Button />
+    </>
+  );
 };
 
 export default Quiz;
